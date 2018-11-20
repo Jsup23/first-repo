d83e0d7fc0e0533cfece96306b4d5084e743cc08
Josiah Caldwell <jscaldwell@aggies.ncat.edu>
2018-11-20 11:03:14 -0500

    Javadocd up

---
+++ Goodbye163.java
@@ -0,0 +1,9 @@
+/**Class to print Goodbye COMP 163.
+ * @author jscaldwell@aggies.ncat.edu
+ */
+
+public class Goodbye163 {
+  public static void main(String[] args) {
+    Sytem.out.println("Goodbye COMP 163");
+  }
+}
